6156a6226bfa12e8a0e99f22e63f1b4da1cf3cc3
Nicolis Yuvienco <nickyuvienco@gmail.com>
2025-02-26 12:50:55 -0800

    fix formatting and add comment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+});
+
+//TODO: Point to src for index.html
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+//
+// // https://vite.dev/config/
+// export default defineConfig({
+//   root: 'src',  // Set the 'src' folder as the root directory
+//   plugins: [react()],
+//   build: {
+//     outDir: '../dist',  // Specify where to output the built files (outside of 'src')
+//   },
+// })
