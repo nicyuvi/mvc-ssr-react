bbcef506036dba5287ab17562126cbb8589b1c68
Nicolis Yuvienco <nickyuvienco@gmail.com>
2025-02-27 17:40:42 -0800

    add a prop drilling example with dynamic data from server

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -2,7 +2,18 @@ import { Route, Switch } from 'react-router-dom';
 import { ServerSideProps } from '../types';
 import './App.css';
 
-const Home = () => <div>Home Page</div>;
+interface HomeProps {
+  user: { id: string; name: string } | undefined;
+}
+
+const Home = ({ user }: HomeProps) => (
+  <>
+    <h1>Home Page</h1>
+    <p>server side props: </p>
+    <p>{user?.id}</p>
+    <p>{user?.name}</p>
+  </>
+);
 const About = () => <div>About Page</div>;
 
 function App({ serverSideProps }: { serverSideProps: ServerSideProps }) {
@@ -11,7 +22,7 @@ function App({ serverSideProps }: { serverSideProps: ServerSideProps }) {
 
   return (
     <Switch>
-      <Route exact path="/" component={Home} />
+      <Route exact path="/" render={() => <Home user={user} />} />
       <Route exact path="/about" component={About} />
     </Switch>
   );
