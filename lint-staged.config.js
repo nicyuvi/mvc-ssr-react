faa0733db4d28cbdc3ee304f3a7910dedcd05237
Nicolis Yuvienco <nickyuvienco@gmail.com>
2025-03-05 20:27:17 -0800

    add typecheck client and server. add typecheck to pre commit hook

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,8 @@
 export default {
   '*.{ts,tsx,js,jsx}': [
+    () => 'npm run typecheck:client',
+    () => 'npm run typecheck:server',
     'eslint --cache --max-warnings=0',
-    () => 'npm run typecheck',
     'prettier --write',
   ],
   '*.{json,css,md}': ['prettier --write'],
